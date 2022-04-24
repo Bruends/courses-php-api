cbbc991f9cb5b77cbbabba4bdcd364a6932d3d5a
Bruno Mendes <brunomg730@gmail.com>
2022-04-23 21:50:03 -0300

    add: configs for database

---
+++ config.example.php
@@ -0,0 +1,11 @@
+<?php
+
+// rename this file to config.php 
+// and add your configurations
+
+// Database config
+define('DB_HOST', 'localhost');
+define('DB_USER', 'user');
+define('DB_PASS', 'password');
+define('DB_NAME',  'CoursesApp');
+
