8b89a0ba99598897f037618084d88dbfcd134aa5
Bruno Mendes <brunomg730@gmail.com>
2022-04-25 10:53:31 -0300

    feat: protecting routes with jwt

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once "config.php";
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Tuupola\Middleware\JwtAuthentication;
 
 use CoursesApi\Controller\AuthController;
 
@@ -14,6 +15,13 @@ $app = AppFactory::create();
 // middlewares
 $app->addBodyParsingMiddleware();
 $app->addRoutingMiddleware();
+
+// jwt auth middleware
+$app->add(new JwtAuthentication([
+  "path" => "/courses",
+  "secret" => JWT_KEY
+]));
+
 
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
@@ -25,5 +33,13 @@ $app->post('/auth/register', function (Request $request, Response $response) {
 $app->post('/auth/login', function (Request $request, Response $response) {
   return AuthController::authenticate($request, $response);
 });
+
+// course routes
+$app->get('/courses', function (Request $request, Response $response) {
+  $response->getBody()->write("courses");
+  return $response;
+});
+
+
 
 $app->run();
